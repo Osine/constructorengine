04dbc3441b2bdf2e92e9c6f8274eaeeebf055f4a
Osine Ikhianosime <osikhianosime@gmail.com>
2025-03-09 13:10:44 +0100

    camera work, the basics

--- src/main/java/com/bludoors/onehundredandonefahrenheitstudios/engine/ecs/core/component/CameraComponent.java
+++ src/main/java/com/bludoors/onehundredandonefahrenheitstudios/engine/ecs/core/component/CameraComponent.java
@@ -3,5 +3,31 @@ package com.bludoors.onehundredandonefahrenheitstudios.engine.ecs.core.component
 import com.bludoors.onehundredandonefahrenheitstudios.engine.ecs.core.Component;
 
 public class CameraComponent extends Component{
+
+    public enum CameraType{
+        PERSPECTIVE,
+        ORTHOGRAPHIC
+    }
+
+    public CameraType cameraType = CameraType.PERSPECTIVE;
+
+    //perspective
+    public float fov = 60.0f;
+    public float nearPlane = 0.1f;
+    public float farPlane = 1000.0f;
+
+    //orthographic
+    public float left = -10.0f;
+    public float right = 10.0f;
+    public float top = 10.0f;
+    public float bottom = -10.0f;
+
+    public boolean isActive = true;
+
+    public CameraComponent(){}
+
+    public CameraComponent(CameraType type){
+        this.cameraType = type;
+    }
     
 }
