b37953e3daf62b938a60740b26c3f9fbb89b587d
Osine Ikhianosime <osikhianosime@gmail.com>
2025-03-09 13:14:24 +0100

    simple component for the rendersystem to take advantage of

--- src/main/java/com/bludoors/onehundredandonefahrenheitstudios/engine/ecs/core/component/RenderComponent.java
+++ src/main/java/com/bludoors/onehundredandonefahrenheitstudios/engine/ecs/core/component/RenderComponent.java
@@ -3,5 +3,17 @@ package com.bludoors.onehundredandonefahrenheitstudios.engine.ecs.core.component
 import com.bludoors.onehundredandonefahrenheitstudios.engine.ecs.core.Component;
 
 public class RenderComponent extends Component{
+
+    public String meshID;
+    public String materialID;
+
+    public boolean isVisible = true;
+
+    public RenderComponent(){}
+
+    public RenderComponent(String meshID, String materialID){
+        this.meshID = meshID;
+        this.materialID =  materialID;
+    }
     
 }
